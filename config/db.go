328b9f491d0e951910633ca55bf1b533c0b93c0d
Uallessonivo <uallessons@gmail.com>
2024-12-19 20:38:52 -0300

    feat: initialize database connection and auto-migrate schemas

---
+++ config/db.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"Github.com/Uallessonivo/RealWorld/internal/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitDatabase() {
+	db, err := gorm.Open(sqlite.Open("realworld.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	if er := db.AutoMigrate(
+		&models.User{},
+		&models.Profile{},
+		&models.Comment{},
+		&models.Article{}); er != nil {
+		panic("failed to migrate schemas")
+	}
+}
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"Github.com/Uallessonivo/RealWorld/cmd"
+	"Github.com/Uallessonivo/RealWorld/config"
 )
 
 func main() {
+	config.InitDatabase()
 	cmd.Execute()
 }
