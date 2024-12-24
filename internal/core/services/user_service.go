dc69693257cc9da200147bfea8519c3bc3ce4310
Uallessonivo <uallessons@gmail.com>
2024-12-24 05:57:51 -0300

    feat: initial implementation for user service methods for creating, finding, and updating users

--- internal/core/services/user_service.go
+++ internal/core/services/user_service.go
@@ -17,20 +17,52 @@ func NewUserService(u ports.UserRepository) ports.UserService {
 
 // CreateUser implements ports.UserService.
 func (u *UserService) CreateUser(model *models.UserRegistrationRequest) (*models.User, error) {
-	panic("unimplemented")
+	user := &models.User{
+		Email:        model.Email,
+		Username:     model.Username,
+		PasswordHash: model.Password,
+	}
+
+	err := u.repo.CreateUser(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 // FindByEmail implements ports.UserService.
 func (u *UserService) FindByEmail(email string) (*models.User, error) {
-	panic("unimplemented")
+	user, err := u.repo.FindByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 // GetUserById implements ports.UserService.
 func (u *UserService) GetUserById(id int) (*models.User, error) {
-	panic("unimplemented")
+	user, err := u.repo.GetUserById(id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 // UpdateUser implements ports.UserService.
 func (u *UserService) UpdateUser(model *models.UserUpdateRequest) (*models.User, error) {
-	panic("unimplemented")
+	user := &models.User{
+		Email:        model.Email,
+		Username:     model.Username,
+		PasswordHash: model.Password,
+		Image:        model.Image,
+		Bio:          model.Bio,
+	}
+
+	err := u.repo.UpdateUser(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
